5676c5dde66fd2e94238b8feef4db1cd42cdd035
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-11-18 13:35:02 +0800

    closes #2

---
+++ src/smartcontract/nativevm/OntidContractTxBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ontio\smartcontract\nativevm;
+
+use ontio\crypto\PublicKey;
+use ontio\crypto\Address;
+use ontio\core\transaction\Transaction;
+use ontio\common\ByteArray;
+use ontio\smartcontract\abi\NativeVmParamsBuilder;
+use ontio\core\transaction\TransactionBuilder;
+use ontio\smartcontract\abi\Struct;
+
+class OntidContractTxBuilder
+{
+  const ONTID_CONTRACT = '0000000000000000000000000000000000000003';
+  public static $ONTID_METHOD;
+
+  public function buildRegisterOntidTx(
+    string $ontid,
+    PublicKey $pubkey,
+    string $gasPrice,
+    string $gasLimit,
+    ? Address $payer = null
+  ) : Transaction {
+    $method = self::$ONTID_METHOD->regIDWithPublicKey;
+
+    if (strpos($ontid, 'did') === 0) {
+      $ontid = ByteArray::fromBinary($ontid)->toHex();
+    }
+
+    $struct = new Struct();
+    $struct->add($ontid, $pubkey->toHex());
+    $list = [$struct];
+
+    $paramsBuilder = new NativeVmParamsBuilder();
+    $paramsBuilder->pushNativeCodeScript($list);
+    $params = $paramsBuilder->toHex();
+
+    $txBuilder = new TransactionBuilder();
+    return $txBuilder->makeNativeContractTx($method, $params, new Address(self::ONTID_CONTRACT), $gasPrice, $gasLimit, $payer);
+  }
+}
+
+OntidContractTxBuilder::$ONTID_METHOD = (object)[
+  'regIDWithPublicKey' => 'regIDWithPublicKey',
+  'regIDWithAttributes' => 'regIDWithAttributes',
+  'addAttributes' => 'addAttributes',
+  'removeAttribute' => 'removeAttribute',
+  'getAttributes' => 'getAttributes',
+  'getDDO' => 'getDDO',
+  'addKey' => 'addKey',
+  'removeKey' => 'removeKey',
+  'getPublicKeys' => 'getPublicKeys',
+  'addRecovery' => 'addRecovery',
+  'changeRecovery' => 'changeRecovery',
+  'getKeyState' => 'getKeyState'
+];
