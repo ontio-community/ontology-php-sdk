60877ff3fcc4150f7e1c02c89a854a302667d596
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-11-21 00:58:06 +0800

    scripts

---
+++ scripts/php-build-cfg.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+./configure --disable-all \
+  --prefix=/usr/local/php \
+  --enable-phar \
+  --enable-session \
+  --enable-short-tags \
+  --enable-tokenizer \
+  --with-pcre-regex \
+  --with-openssl --with-openssl-dir=/usr/local/bin \
+  --with-bz2 \
+  --with-zlib\
+  --with-gmp=/usr/local \
+  --enable-dom \
+  --enable-libxml \
+  --enable-simplexml \
+  --enable-xml \
+  --enable-xmlreader \
+  --enable-xmlwriter \
+  --with-xsl \
+  --with-libxml-dir \
+  --enable-opcache \
+  --enable-bcmath \
+  --enable-calendar \
+  --enable-cli \
+  --enable-ctype \
+  --enable-dom \
+  --enable-fileinfo \
+  --enable-filter \
+  --enable-shmop \
+  --enable-sysvsem \
+  --enable-sysvshm \
+  --enable-sysvmsg \
+  --enable-json \
+  --enable-mbregex \
+  --enable-mbstring \
+  --with-mhash=/usr/local \
+  --enable-pcntl \
+  --with-pcre-regex \
+  --with-pcre-dir=/usr/local \
+  --enable-pdo \
+  --with-pear \
+  --enable-posix \
+  --with-readline \
+  --enable-sockets \
+  --enable-tokenizer \
+  --with-curl= \
+  --enable-zip
